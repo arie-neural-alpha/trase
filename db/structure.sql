8358747eb8b08b71ebdb0a274e828d9ba7d7980e
Agnieszka Figiel <agnieszka.figiel@gmail.com>
2020-05-21 09:44:53 +0200

    Added subnational_years to contexts

--- db/structure.sql
+++ db/structure.sql
@@ -2072,7 +2072,8 @@ CREATE TABLE public.contexts (
     commodity_id integer NOT NULL,
     years integer[],
     default_year integer,
-    created_at timestamp without time zone NOT NULL
+    created_at timestamp without time zone NOT NULL,
+    subnational_years integer[]
 );
 
 
@@ -17263,6 +17264,7 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20200425173940'),
 ('20200430115447'),
 ('20200501152755'),
-('20200505120049');
+('20200505120049'),
+('20200521074053');
 
 
